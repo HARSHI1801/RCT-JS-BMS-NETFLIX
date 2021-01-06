a948ec51d3ae806ba3672ba3b79b4a37d15ee482
Harshita <miksshukla@gmail.com>
2021-01-06 13:35:10 +0530

    phla

---
+++ src/Sd.jsx
@@ -0,0 +1,59 @@
+const Sd = [
+    {
+        id: 1,
+        imgsrc: "https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781608878277/supernatural-the-poster-collection-9781608878277_hr.jpg",
+        title: "A Netflix Original Series",
+        sname:"SUPERNATURAL",
+        review: "https://www.quora.com/What-is-your-review-of-Supernatural-TV-series",
+        link: "https://www.netflix.com/in/Login?nextpage=https%3A%2F%2Fwww2.netflix.com%2Ftitle%2F60027997",
+        
+    },
+    {
+        id: 2,
+        imgsrc:"https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg",
+        title: "IMDB 10/10 MOVIE",
+        sname:"HARRY POTTER",
+        review: "https://medium.com/media-authority/harry-potter-movies-comprehensive-analysis-review-a3a58fac1605",
+        link: "https://www.hbo.com/movies/harry-potter",
+
+    },
+    {
+        id: 3,
+        imgsrc: "https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=1350&h=707&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2019%2F06%2Fstrangerthings_s3-2000.jpg",
+        title: "A Netflix Original Series",
+        sname: "STRANGER THINGS",
+        review: "https://www.quora.com/What-is-your-review-of-Stranger-Things",
+        link: "https://www.netflix.com/in/Login?nextpage=https%3A%2F%2Fwww2.netflix.com%2Ftitle%2F60027997",
+    },
+    {
+        id: 4,
+        imgsrc: "https://cdn.europosters.eu/image/750/posters/the-vampire-diaries-woods-i21244.jpg",
+        title: "A Netflix Original Series",
+        sname: "VAMPIRE DIARIES",
+        review: "https://www.quora.com/What-is-your-review-of-The-Vampire-Diaries-TV-series",
+        link: "https://www.netflix.com/in/Login?nextpage=https%3A%2F%2Fwww2.netflix.com%2Ftitle%2F60027997",
+
+    },
+    {
+        id: 5,
+        imgsrc: "https://ih1.redbubble.net/image.1157224839.7381/flat,750x,075,f-pad,750x1000,f8f8f8.jpg",
+        title: "IMDB 10/10 MOVIE",
+        sname: "500 DAYS OF SUMMER ",
+        review: "https://www.quora.com/What-did-you-find-the-most-meaningful-in-500-Days-of-Summer-and-how-did-you-feel-about-the-ending",
+        link: "https://www.netflix.com/in/Login?nextpage=https%3A%2F%2Fwww2.netflix.com%2Ftitle%2F60027997",
+
+
+    },
+    {
+        id: 6,
+        imgsrc: "https://m.media-amazon.com/images/M/MV5BMTg0NjEwNjUxM15BMl5BanBnXkFtZTcwMzk0MjQ5Mg@@._V1_.jpg",
+        title: "IMDB 10/10 MOVIE",
+        sname: "SHERLOCK HOLMES",
+        review: "https://www.quora.com/What-is-your-review-of-Sherlock-Holmes",
+        link: "https://www.netflix.com/in/Login?nextpage=https%3A%2F%2Fwww2.netflix.com%2Ftitle%2F60027997",
+        
+    }
+]
+
+
+export default Sd;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Sd from './Sd';
+
+function Card(props){
+  return (
+    <>
+  <div className="cards">
+    <div className="card">
+      <img src={props.imgsrc}
+      alt="myPic"
+        width="400" height="380" />
+        <div className="divir" >{props.title}</div>
+        <div className="divir"><h3>{props.sname}</h3></div>
+        <a href={props.review} target="_blank">
+        <div className="divi"><button>READ REVIEWS NOW !</button></div>
+        </a>
+
+        <a href={props.link} target="_blank">
+        <div className="divi" ><button>WATCH NOW !</button></div>
+        </a>
+        
+    </div>
+
+  </div></>
+  );
+}
+
+function netcd(d) {
+ return (
+ <Card
+ key={d.id}
+  imgsrc={d.imgsrc}
+  title={d.title}
+  sname={d.sname}
+  review={d.review}
+  link={d.link}
+
+  />
+);
+
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+  
+<div className="heading_style">BEST IMDB RATED MOVIES & BEST NETFLIX SERIES</div>
+{Sd.map(netcd)}
+  </>
+  ,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
